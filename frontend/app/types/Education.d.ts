61332cc2f452ebba18c0226d914a9e90872c7d4e
DevJ2K <ajavontheopro@gmail.com>
2025-06-16 21:33:31 +0200

    ✅ : Education type...

---
+++ frontend/app/types/Education.d.ts
@@ -0,0 +1,17 @@
+export interface Education {
+  title: string;
+  image: string;
+
+  borderColor: string;
+
+  startDate: string;
+  endDate?: string;
+
+  stat: string;
+  description: string;
+
+  btnIcon: string;
+  btnText: string;
+  btnActive: boolean;
+  link?: string;
+}
