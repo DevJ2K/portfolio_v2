2ac663b9e708f01ef434f4279ce928bbd906c2aa
DevJ2K <ajavontheopro@gmail.com>
2025-06-18 22:44:19 +0200

    ✅ : Able to filter projects.

--- frontend/app/components/view/Projects.vue
+++ frontend/app/components/view/Projects.vue
@@ -11,24 +11,45 @@
       @select="(index) => selectedTag = index" />
 
     <div class="flex flex-wrap justify-center gap-8">
+
       <ProjectCard
-        title="SnakeAI"
-        description="The goal of this project is to introduce basic of reinforcement learning with a snake game. The snake has to reach a minimum length of 10."
-        online-link="https://snakeai.devj2k.com"
-        github-name="SnakeAI"
-        image="/images/projects/snake_ai.png"/>
-      <ProjectCard
-        title="Gomoku"
-        description="The goal of this project is to make an AI capable of beating human players at Gomoku."
-        github-name="app_gomoku"
-        image="/images/projects/app_gomoku.png"/>
+        v-for="(project, index) in projects.filter(proj => proj.tags.includes(selectedTag))"
+        :key="index"
+        :title="project.title"
+        :description="project.description"
+        :online-link="project.onlineLink"
+        :github-name="project.githubLink ? project.githubLink.split('/').pop() : ''"
+        :image="project.image" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Project } from '~/types/Project';
+
 const tags = ['AI', 'Web', 'Mobile'];
 const selectedTag = ref(tags[0]);
+
+const projects: Project[] = [
+  {
+    title: 'SnakeAI',
+    description: 'The goal of this project is to introduce basic of reinforcement learning with a snake game. The snake has to reach a minimum length of 10.',
+    image: '/images/projects/snake_ai.png',
+    tags: ['AI'],
+
+    onlineLink: 'https://snakeai.devj2k.com',
+    githubLink: 'https://github.com/DevJ2K/SnakeAI',
+  },
+  {
+    title: 'Gomoku',
+    description: 'The goal of this project is to make an AI capable of beating human players at Gomoku.',
+    image: '/images/projects/app_gomoku.png',
+    tags: ['AI'],
+
+    githubLink: 'https://github.com/DevJ2K/app_gomoku',
+  }
+]
+
 </script>
 
 <style scoped>
---
+++ frontend/app/types/Project.d.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  title: string;
+  description: string;
+  image: string;
+
+  tags: string[];
+
+  onlineLink?: string;
+  githubLink?: string;
+}
