f3a6029d1c5342ab42937d9c17edb0a1a128c3f2
DevJ2K <ajavontheopro@gmail.com>
2025-06-22 23:11:12 +0200

    ✅ : Add discord notification provider to API

--- backend/app/routes/contact/contact.py
+++ backend/app/routes/contact/contact.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 from app.models.API.Models import ContactModel
 from app.services.ContactService import ContactService
 from app.configuration.Configuration import CONFIGURATION
+from app.services.Contact.Providers import DiscordProvider
 
 
 router = APIRouter()
@@ -9,7 +10,10 @@ router = APIRouter()
 contact_service = ContactService(
     email_receiver=CONFIGURATION.EMAIL_RECEIVER,
     email_sender=CONFIGURATION.EMAIL_SENDER,
-    password_sender=CONFIGURATION.PASSWORD_SENDER)
+    password_sender=CONFIGURATION.PASSWORD_SENDER,
+    providers=[
+        DiscordProvider(webhook_url=CONFIGURATION.DISCORD_WEBHOOK_URL)
+])
 
 @router.post('/send', description="Send a contact message")
 async def contact(body: ContactModel):
