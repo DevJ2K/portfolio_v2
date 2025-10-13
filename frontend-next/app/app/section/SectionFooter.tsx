b377ca4f4e07d25a4631fdd6c3ff3a78bfc5591f
DevJ2K <ajavontheopro@gmail.com>
2025-10-13 12:03:57 +0200

    ✅ : Add footer

---
+++ frontend-next/app/app/section/SectionFooter.tsx
@@ -0,0 +1,14 @@
+const SectionFooter = () => {
+  return (
+    <footer className="flex flex-col items-center justify-center text-center min-h-32 mb-16 sm:mb-0 text-foreground-secondary">
+      <h1 className="text-lg font-medium leading-8 text-nowrap">
+        Built by DevJ2K
+      </h1>
+      <p className="text-base text-center text-nowrap">
+        Copyright © Version 2025.10 - All Rights Reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default SectionFooter;
