3e8e6ca4f6d07f7b571752fec0f75a366b8cd7d5
DevJ2K <ajavontheopro@gmail.com>
2025-10-14 20:07:40 +0200

    📝 : Update skills...

--- frontend-next/app/utils/IconMap.tsx
+++ frontend-next/app/utils/IconMap.tsx
@@ -1,5 +1,38 @@
-import { FaPython } from "react-icons/fa";
+import {
+  RiFirebaseLine,
+  RiJavaLine,
+  RiNextjsLine,
+  RiReactjsLine,
+} from "react-icons/ri";
+import {
+  SiC,
+  SiCplusplus,
+  SiFastapi,
+  SiNuxtdotjs,
+  SiTailwindcss,
+} from "react-icons/si";
+import {
+  TbBrandDocker,
+  TbBrandPython,
+  TbBrandSupabase,
+  TbBrandSwift,
+  TbBrandTypescript,
+} from "react-icons/tb";
 
 export const ICON_MAP: { [key: string]: React.ReactNode } = {
-  "python": <FaPython />,
+  python: <TbBrandPython />,
+  nuxt: <SiNuxtdotjs />,
+  java: <RiJavaLine />,
+  swift: <TbBrandSwift />,
+  next: <RiNextjsLine />,
+  supabase: <TbBrandSupabase />,
+  firebase: <RiFirebaseLine />,
+  docker: <TbBrandDocker />,
+  fastapi: <SiFastapi />,
+  typescript: <TbBrandTypescript />,
+  "c++": <SiCplusplus />,
+  c: <SiC />,
+  tailwindcss: <SiTailwindcss />,
+  react: <RiReactjsLine />,
+  "react native": <RiReactjsLine />,
 };
